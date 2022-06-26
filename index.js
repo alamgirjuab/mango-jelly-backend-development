e84acb8b9853cbd1e851c552c7433ff7bb9dffdc
alamgirjuab <alamgirjuab@gmail.com>
2022-06-27 00:57:51 +0600

    crud complete

--- index.js
+++ index.js
@@ -31,6 +31,52 @@ async function run() {
         const allPhones = await cursor.toArray();
         res.send(allPhones);
     });
+
+    // POST API
+    app.post('/products', async (req, res) => {
+        const data = req.body;
+        console.log('hit the post api', data);
+
+        const result = await phoneItems.insertOne(data);
+        console.log(result);
+        res.json(result);
+    });
+
+    // DELETE API
+    app.delete('/products/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await phoneItems.deleteOne(query);
+        res.json(result);
+
+    })
+
+    // PUT API (Update API)
+
+    app.put('/products/:id', async (req, res) => {
+        const id = req.params.id;
+        console.log('updating user', id);
+        // res.send('update not user')
+        const updateService = req.body;
+        console.log(updateService);
+        const filter = { _id: ObjectId(id) };
+        const options = { upsert: true };
+        const updateDoc = {
+            $set: {
+                brand_name: updateService.brand_name,
+                model: updateService.model,
+                ram: updateService.ram,
+                internal_storage: updateService.internal_storage,
+                screen_size: updateService.screen_size,
+                image: updateService.image,
+                spec: updateService.spec,
+                price: updateService.price
+            },
+        };
+        const result = await phoneItems.updateOne(filter, updateDoc, options);
+        // console.log()
+        res.json(result)
+    })
 }
 
 run().catch(console.dir);
